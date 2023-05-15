3d606390af7ce146c1ba9837bcaa387520468b5b
Mariano Torres <marianxtorres@gmail.com>
2023-05-14 21:38:44 -0300

    add target blank

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -11,7 +11,7 @@ const Project = () => {
                     <div className={style.containerPerProject}>
                         <div className={style.project}>
                             <h2 className={style.titleProject}>Videogames App</h2>
-                            <a href='https://pi-videogames-mdjn.vercel.app/'><img className={style.img} width='550px' src="https://res.cloudinary.com/djdqwkavb/image/upload/v1684008036/Captura_ktt5bg.png"></img></a>
+                            <a href='https://pi-videogames-mdjn.vercel.app/' target="_blank"><img className={style.img} width='550px' src="https://res.cloudinary.com/djdqwkavb/image/upload/v1684008036/Captura_ktt5bg.png"></img></a>
                         </div>
                         <div className={style.info}>
                             <p>Single-page application que consume información sobre videojuegos de la API RAWG, la misma cuenta con las siguiente funcionalidades:
@@ -30,7 +30,7 @@ const Project = () => {
                     <div className={style.containerPerProject}>
                         <div className={style.project}>
                             <h2 className={style.titleProject}>The Chocolate Hub</h2>
-                            <a href='https://deploy-front2.vercel.app/home'><img className={style.img} width='550px' src="https://res.cloudinary.com/djdqwkavb/image/upload/v1684008128/Captura_xfpyjd.png"></img></a>
+                            <a href='https://deploy-front2.vercel.app/home' target="_blank"><img className={style.img} width='550px' src="https://res.cloudinary.com/djdqwkavb/image/upload/v1684008128/Captura_xfpyjd.png"></img></a>
                         </div>
                         <div className={style.info}>
                             <p>E-commerce de venta de chocolates, ofrece una amplia variedad de productos de alta calidad. Aceptamos pasarelas seguras de pago como MercadoPago y criptomonedas, y ofrecemos un registro fácil con cuentas de Google o Facebook. Los usuarios pueden valorar y comentar productos, y los administradores tienen acceso a un dashboard completo para modificar cualquier aspecto de la aplicación.</p>
@@ -43,7 +43,7 @@ const Project = () => {
                     <div className={style.containerPerProject}>
                         <div className={style.project}>
                             <h2 className={style.titleProject}>Portfolio</h2>
-                            <a href=''><img className={style.img} width='550px' src="https://res.cloudinary.com/djdqwkavb/image/upload/v1684021017/Captura_cxq0w9.png"></img></a>
+                            <a href='' ><img className={style.img} width='550px' src="https://res.cloudinary.com/djdqwkavb/image/upload/v1684021017/Captura_cxq0w9.png"></img></a>
                         </div>
                         <div className={style.info}>
                             <p>Mi Portfolio :D</p>
