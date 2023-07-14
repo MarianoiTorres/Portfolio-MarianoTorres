2185f8706d760b0767763bb420383d2197efeb8c
Mariano Torres <marianxtorres@gmail.com>
2023-07-14 18:34:35 -0300

    nuevas tecnologias

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,4 +1,5 @@
 import style from './Skills.module.css'
+import ts from './ts.png'
 
 const Skills = () => {
 
@@ -18,8 +19,10 @@ const Skills = () => {
                     <div className={style.skills}><img className={style.img} src="https://img.icons8.com/ios/50/react-native--v1.png" alt="react-native--v1"/>React</div>
                     <div className={style.skills}><img className={style.img} src="https://img.icons8.com/ios/50/redux.png" alt="redux"/>Redux</div>
                     <div className={style.skills}><img className={style.img} src="https://img.icons8.com/ios/50/git.png" alt="git"/>Git</div>
-                    <div className={style.skills}><img className={style.img} src="https://img.icons8.com/ios/50/css3.png" alt="css3"/>CSS</div>
+                    <div className={style.skills}><img className={style.img} src="https://img.icons8.com/ios/50/css3.png" alt="css3"/>CSS</div>  
                     <div className={style.skills}><img className={style.img} src="https://cdn.iconscout.com/icon/free/png-256/tailwind-css-5285308-4406745.png" alt="css3"/>Tailwind</div>
+                    <div className={style.skills}><img className={style.img} src="https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/mongodb-512.png" alt="css3"/>MongoDB</div>
+                    <div className={style.skills}><img className={style.img} src='https://img.icons8.com/ios/50/typescript.png' alt="css3"/>Typescript</div>
                 </div>
             </div>
         </div>
