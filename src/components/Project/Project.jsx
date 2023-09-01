0d61ac078b615270c3c32ef5146156105e7f979e
Mariano Torres <marianxtorres@gmail.com>
2023-09-01 01:19:32 -0300

    trello

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -10,25 +10,6 @@ const Project = () => {
                 <div className={style.projectsContainer}>
                     <div className={style.containerPerProject}>
                         <div className={style.project}>
-                            <h2 className={style.titleProject}>Videogames App</h2>
-                            <a href='https://pi-videogames-mdjn.vercel.app/' target="_blank"><img className={style.img} width='550px' src="https://res.cloudinary.com/djdqwkavb/image/upload/v1684008036/Captura_ktt5bg.png"></img></a>
-                        </div>
-                        <div className={style.info}>
-                            <p>Single-page application que consume información sobre videojuegos de la API RAWG, la misma cuenta con las siguiente funcionalidades:
-                                - Paginado
-                                - Filtrar por género u origen
-                                - Ordenar por rating o alfabéticamente
-                                - Buscar juegos
-                                - Crear tu propio juego
-                                - Ver los detalles de cada uno al hacer click sobre las cards</p>
-                            <div className={style.links}>
-                                <button className={style.demo}><a className={style.a} href="https://pi-videogames-mdjn.vercel.app/" target="_blank">Ver demo</a></button>
-                                <button className={style.github}><a href="https://github.com/MarianoiTorres/PI-Videogames" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" /></a></button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={style.containerPerProject}>
-                        <div className={style.project}>
                             <h2 className={style.titleProject}>Trello Clone</h2>
                             <a href='https://trello-clone-two-rouge.vercel.app/' target="_blank"><img className={style.img} width='550px' src="https://res.cloudinary.com/djdqwkavb/image/upload/v1693277282/1_udblnq.png"></img></a>
                         </div>
@@ -50,6 +31,25 @@ const Project = () => {
                             <div className={style.links}>
                                 <button  className={style.demo}><a className={style.a} href='https://deploy-front2.vercel.app/home' target="_blank">Ver demo</a></button>
                                 <button className={style.github}><a href="https://github.com/MarianoiTorres/PF-TheChocolateHub" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" /></a></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.containerPerProject}>
+                        <div className={style.project}>
+                            <h2 className={style.titleProject}>Videogames App</h2>
+                            <a href='https://pi-videogames-mdjn.vercel.app/' target="_blank"><img className={style.img} width='550px' src="https://res.cloudinary.com/djdqwkavb/image/upload/v1684008036/Captura_ktt5bg.png"></img></a>
+                        </div>
+                        <div className={style.info}>
+                            <p>Single-page application que consume información sobre videojuegos de la API RAWG, la misma cuenta con las siguiente funcionalidades:
+                                - Paginado
+                                - Filtrar por género u origen
+                                - Ordenar por rating o alfabéticamente
+                                - Buscar juegos
+                                - Crear tu propio juego
+                                - Ver los detalles de cada uno al hacer click sobre las cards</p>
+                            <div className={style.links}>
+                                <button className={style.demo}><a className={style.a} href="https://pi-videogames-mdjn.vercel.app/" target="_blank">Ver demo</a></button>
+                                <button className={style.github}><a href="https://github.com/MarianoiTorres/PI-Videogames" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" /></a></button>
                             </div>
                         </div>
                     </div>
